4eb4de369915a384a4c24a5c5b3eddd38a857fe3
John Norwood <norwoodj@gmail.com>
2020-07-10 12:33:19 -0500

    initial commit

---
+++ discuss/lib/discuss_web/controllers/topic_controller.ex
@@ -0,0 +1,9 @@
+defmodule Discuss.TopicController do
+
+    use DiscussWeb, :controller
+    def new(conn, params) do
+
+
+    end    
+
+end
---
+++ phoenix/lib/phoenix.ex
@@ -0,0 +1,18 @@
+defmodule Phoenix do
+  @moduledoc """
+  Documentation for `Phoenix`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Phoenix.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
